29ef322eadf39dbef562ff06ba98156aa6c86e94
Dan Shoutis <dan@shoutis.org>
2010-06-16 16:32:37 -0600

    About to delete

--- sgame.js
+++ sgame.js
@@ -1,51 +1,108 @@
-// simplistic eye candy for the good ol' resume
+// eye candy for the good ol resume. 
 //  see: contextfreeart.org
 
-vect_x_mat = function(v,m) {
-   return [v[0] * m[0] + v[0] * m[1] + m[4],
-	   v[1] * m[2] + v[1] * m[3] + m[5]];
-}
 
+MAXD=60;
 
+TimeAware = function(inner) {
+   // Transform a rule to support expressions-over-time in its args.
+   return function() {
+      var _this = this;
+      var a = arguments;
+      var dv = [];
+      var haveone = false;;
+      for (i = 0; i < a.length; i++) {
+	 if (typeof(a[i]) == 'string') {
+	    a[i] = new Function(["t"], "return " + a[i] + ";");
+	    dv.push(true);
+	    haveone = true;
+	 } else {
+	    dv.push(false);
+	 }
+      }
 
+      if (!haveone) 
+	 return inner.apply(this,arguments);
 
+      return function(c) {
+	 var na = [];
 
-Context = function() { return {
-   tx : [1,0,0,1,0,0], /* txformation */
-   color : [0,0,0], /* color */
-   screenw : 0,
-   screenh : 0,
-   tx_stack : [],
-   color_stack : [],
-   push : function() {
-      this.color_stack.push(this.color);
-      this.tx_stack.push(this.tx);
-   },
-   pop : function() {
-      this.color = this.color_stack.pop();
-      this.tx = this.tx_stack.pop();
-   }
+	 for (i = 0; i < a.length; i++) {
+	    if (dv[i]) {
+	       na.push(a[i](c.time));
+	    } else { na.push(a[i]); }
+	 }
+	 var consed = inner.apply(_this,na);
+	 return consed(c);
+      };
+   };
 };
-}
-Rule = function(body) { /* An l-system rule. */
-   
-   can_expand = function(view_matrix, screenw, screenh) {
-      a = vect_x_mat([0,0],view_matrix)
-      b = vect_x_mat([1,1],view_matrix)
-      da = b[0] - a[0];
-      db = b[1] - a[1];
+
+Square = function(c) {  c.fillRect(0,0,1,1); };
+
+Tx = TimeAware(function(matrix, inner) {
+   var enter = function(c) { c.save(); }
+   var exit = function(c) { c.restore(); }
+   return function(c) {
+      enter(c);
+      c.transform(matrix);
+      inner(c);
+      exit(c);
+   };
+});
+
+Rot = TimeAware(function(ang, inner) {
+   var yv = Math.sin(ang);
+   var xv = Math.cos(ang);
+
+   // new x axis = [xv,yv];
+   // new y axis = [-yv,xv];
+   return Tx([xv,yv,-yv,xv,0,0],inner);
+});
+
+Mv = TimeAware(function(x,y,inner) {
+   return Tx([1,0,0,1,x,y], inner);
+});
+
+Sc = TimeAware(function(xamt,yamt,inner) {
+   return Tx([xamt,0,0,yamt,0,0],inner);
+});
+
+Rand = function(inners) {
+   // Flip a coin. Trick: somehow arrive at a stable number...
+};
+
+M = function(inners) {
+   return function(ctx) {
+      for (i = 0; i < inners.length; i++) {
+	 inners[i](ctx);
+      }
+   };
+};
+
+Rule = function(inner) {
+   can_expand = function(ctx, view_matrix, screenw, screenh) {
+      var a = vect_x_mat([0,0],view_matrix)
+      var b = vect_x_mat([1,1],view_matrix)
+      var da = b[0] - a[0];
+      var db = b[1] - a[1];
       
-      szsq = (da * da + db * db);
-      
+      var sizesq = (da * da + db * db);
+
+      // Time is less than zero:
+      if (ctx.time < 0.0)
+	 return;
+
       // Too small to expand:
       if (sizesq < 3.0)
 	 return; 
 
       // Offscreen: Dont bother expanding.
-      x = Math.min(a[0],b[0]);
-      xx = Math.max(a[0],b[0]);
-      y = Math.min(a[1],b[1]);
-      yy = Math.max(a[1],b[1]);
+      var x = Math.min(a[0],b[0]);
+      var xx = Math.max(a[0],b[0]);
+      var y = Math.min(a[1],b[1]);
+      var yy = Math.max(a[1],b[1]);
+
       if (xx <= 0 ||
 	  yy <= 0 ||
 	  x >= screenw ||
@@ -55,70 +112,75 @@ Rule = function(body) { /* An l-system rule. */
       return true;
    };
 
-   expand = function(c,rulesdict) {
-      res = []
-      if (can_expand(c.tx, c.screenw, c.screenh)) {
-	 for (x in body) {
-	    x.enter(c);
-	    expanded = x.expand(c,rulesdict);
-	    res.append(expanded);
-	    x.exit(c);
-	 }
-      } else {
-	 return [this]; 
+   return function(c) {
+      // check recursion depth:
+
+      if (c.tx_stack.length > MAXD)
+	 return;
+
+      // check onscreen size:
+      if (can_expand(c, c.tx, c.screenw, c.screenh)) {
+	 return inner(c);
       }
    };
+};
 
-   return {
-      expand_to : function(view_matrix, screenw, screenh, ruledict) {
-
-
-	 new_args = []
-	 for (a in args) {
-	    if (typeof(a) == 'string') {
-	       expanded = rules[a](
-	    } else {
-	       new_args.push(a)
-	    }
-	 }
-      },
-      draw : function(ctx) {
-	 ctx.save();
-	 for (a in args) {
-	    if (typeof(a) == 'function') a(ctx);
-	 }
-	 ctx.restore();
-      }
+Context = function(ctx,w,h) { return {
+   tx : [1,0,0,1,0,0], /* txformation */
+   color : [0,0,0], /* color */
+   screenw : w,
+   screenh : h,
+   time : 0.0,
+   fillRect : function(x,y,xx,yy) { return ctx.fillRect(x,y,xx,yy); } ,
+   tx_stack : [],
+   color_stack : [],
+   time_stack : [],
+   save : function() {
+      this.color_stack.push(this.color);
+      this.tx_stack.push(this.tx);
+      this.time_stack.push(this.time);
+      ctx.save();
+   },
+   restore : function() {
+      this.color = this.color_stack.pop();
+      this.tx = this.tx_stack.pop();
+      this.time = this.time_stack.pop();
+      ctx.restore();
+   },
+   transform : function(mat) {
+      this.tx = mat_x_mat(mat, this.tx);
+      ctx.transform.apply(ctx,mat);
    }
+};
+}
 
-   return {
-      matrix : [1,0,0,1,0,0],
-      color : [0,0,0],
-      draw : function(ctx) {
-	 ctx.save();
-	 for c in args
-	 ctx.transform.apply(ctx, this.matrix);
-	 ctx.fillStyle = "rgb(0,0,0)"; // fixme
-	 ctx.fillRect(0,0,1,1);
-	 ctx.restore();
-      },
-      bbox : function() {
-	 
-      }
-   }
+Simple = function(ctx) {
+   return M([
+	     Sc(0.5,1.0,Square),
+	     Rot("-1.0 * t * 0.01",Mv(0.0,0.25,Sc(0.95,0.95,Rule(Simple))))
+	    ])(ctx);
 }
 
 G = (function() {
    var c = document.getElementById("eyecandy");
    var ctx = c.getContext('2d');
    
-   var dummy = LS()
-   dummy.matrix = [100,0,0,100,10,10];
-   
+   var dummy = Tx([100,0,0,100,10,10],Rule(Simple));
+
+   var t = 0.0;
+   var interval = (1.0 / 60.0);
+
+   var context = Context(ctx,200,400);
    var draw = function() {
-      dummy.draw(ctx);
+      ctx.fillStyle = "rgb(255,255,255)";
+      ctx.fillRect(0,0,200,400);
+      ctx.fillStyle = "rgb(0,0,0)";
+      t += interval;
+      context.time = t;
+      dummy(context);
    }
 
+   //setInterval(draw,1000.0 * interval);
    draw();
 
 });
